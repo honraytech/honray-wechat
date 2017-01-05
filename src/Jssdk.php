b7c01b316992f48c62cb0aa69aec81486df5f0a9
honraytech <tech@honraytech.com>
2017-01-05 21:37:42 +0800

    修改

--- src/Jssdk.php
+++ src/Jssdk.php
@@ -48,7 +48,7 @@ class Jssdk {
 
   private function getJsApiTicket() {
     // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode($this->get_php_file("jsapi_ticket.php"));
+    $data = json_decode($this->get_php_file(VENDOR_PATH."/honray/tp5-wechat/src/jsapi_ticket.php"));
     if ($data->expire_time < time()) {
       $accessToken = $this->getAccessToken();
       // 如果是企业号用以下 URL 获取 ticket
@@ -59,7 +59,7 @@ class Jssdk {
       if ($ticket) {
         $data->expire_time = time() + 7000;
         $data->jsapi_ticket = $ticket;
-        $this->set_php_file("jsapi_ticket.php", json_encode($data));
+        $this->set_php_file(VENDOR_PATH."/honray/tp5-wechat/src/jsapi_ticket.php", json_encode($data));
       }
     } else {
       $ticket = $data->jsapi_ticket;
@@ -70,7 +70,7 @@ class Jssdk {
 
   private function getAccessToken() {
     // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
-    $data = json_decode($this->get_php_file("access_token.php"));
+    $data = json_decode($this->get_php_file(VENDOR_PATH."/honray/tp5-wechat/src/access_token.php"));
     if ($data->expire_time < time()) {
       // 如果是企业号用以下URL获取access_token
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
@@ -80,7 +80,7 @@ class Jssdk {
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
-        $this->set_php_file("access_token.php", json_encode($data));
+        $this->set_php_file(VENDOR_PATH."/honray/tp5-wechat/src/access_token.php", json_encode($data));
       }
     } else {
       $access_token = $data->access_token;
