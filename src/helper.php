996a797981d6da3c6401307fc8fdcdedca65234b
honraytech <tech@honraytech.com>
2017-01-06 10:25:50 +0800

    更新
    更新

--- src/helper.php
+++ src/helper.php
@@ -3,29 +3,38 @@ use think\wechat\WechatAuth;
 use think\wechat\Jssdk;
 
 /**
- * [get_wechat_token 获取微信token和openid]
+ * [get_wechat_url 获取微信授权跳转url]
  * @linchuangbin
  * @DateTime  2017-01-05T17:22:06+0800
  * @param     [string]       $appid     [微信配置]
  * @param     [string]       $appsecret [微信配置]
  * @param     [string]       $url       [回调url]
- * @param     [string]       $code      [微信回调code]
+ * @param     [string]       $type      [授权方式]
+ * @return    [string]                  [跳转url]
  */
-function get_wechat_token($appid, $appsecret, $url = '', $code = '')
+function get_wechat_url($appId, $appSecret, $url, $type = 'snsapi_userinfo')
 {
-
-    $auth = new WechatAuth($appid, $appsecret);
-
-    if ($code) {
-        $ret = $auth->getAccessToken('code',$code);
-        $url = $url.'?openid='.$ret['openid'].'&token='.$ret['access_token'];
-        return $url;
-    }
-
-    $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    $url = $auth->getRequestCodeURL($url,'snsapi_base','snsapi_userinfo');
+    $auth = new WechatAuth($appId, $appSecret);
+    $url = $auth->getRequestCodeURL($url, 'snsapi_base', $type);
     return $url;
 }
+
+/**
+ * [get_wechat_token 通过code换取网页授权access_token]
+ * @linchuangbin
+ * @DateTime  2017-01-05T17:22:06+0800
+ * @param     [string]       $appid     [微信配置]
+ * @param     [string]       $appsecret [微信配置]
+ * @param     [string]       $code      [通过授权获取的code参数]
+ * @return    [array]                   [token，openid等信息]
+ */
+function get_wechat_token($appId, $appSecret, $code)
+{
+    $auth = new WechatAuth($appId, $appSecret);
+    $ret = $auth->getAccessToken('code', $code);
+    return $ret;
+}
+
 
 /**
  * [get_wechat_userInfo 获取微信授权用户基本信息]
